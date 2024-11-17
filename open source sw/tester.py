bc66d1f330fa8e0c6d55ab057c2a597a1ab5e730
thee1009 <bada20k@naver.com>
2024-11-17 23:23:41 +0900

    Add files via upload

---
+++ open source sw/tester.py
@@ -0,0 +1,11 @@
+import utils
+
+# calculate the length of hypotenuse (c) when a=3 and b=4
+a, b = 3, 4
+c = utils.pythagoras(a, b)  # 함수 이름 pythagoras로 수정
+print('c =', c)
+
+# calculate the area of circle with radius r = 10
+r = 10
+area = utils.circle(r)  # 함수 이름 circle로 수정
+print('area =', area)
